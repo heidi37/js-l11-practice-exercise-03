50c026fec04a443568b7111ae6c0c0adf910cc11
Heidi Fryzell <heidifryzell@MacBook-Air-2.local>
2021-05-17 15:02:54 -0400

    working code

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
 const notRegistered = document.querySelector(".not-registered");
 
-const getRegistrationDate = async function () {
+const getRegistrationData = async function () {
     const request = await fetch ("https://gist.githubusercontent.com/skillcrush-curriculum/4bfa96d3762f86d2327d389063b3ea8d/raw/86e50e48e98c662ead6d9bd191ef7db381e9f4b4/fieldtrip.json");
-    const data = request.json();
+    const data = await request.json();
     displayContactList(data);
 };
 
-const displayContactList(data) {
-    for (let student of data) {
-     let listItem = document.createElement("li");
-     listItem.InnerText = data.name;
-     notRegistered.append(listItem);
+const displayContactList = function (data) {
+    for (const student of data) {
+        if (student.registered === "no"){
+        const listItem = document.createElement("li");
+        listItem.innerText = student.name;
+        notRegistered.append(listItem);
+        }
     }
 };
 
